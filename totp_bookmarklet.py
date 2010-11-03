a0a50e9e9baf3848aacfe95f30c55392784406c3
Benjamin Dauvergne <bdauvergne@entrouvert.com>
2010-11-03 03:05:12 +0100

    reformat package file

--- totp_bookmarklet.py
+++ totp_bookmarklet.py
@@ -1,10 +1,13 @@
 import binascii
 import base64
+import os.path
 
+def __content(f):
+    return open(os.path.join(os.path.dirname(__file__), f)).read()
 
-crypto_js = open('crypto.js').read()
-hotp_js = open('hotp.js').read()
-myotp_js = open('my-otp.js').read()
+crypto_js = __content('crypto.js')
+hotp_js = __content('hotp.js')
+myotp_js = __content('my-otp.js').
 
 
 def dataize(document, type='text/html'):
@@ -12,7 +15,11 @@ def dataize(document, type='text/html'):
 
 def otp_doc(secret):
     key = binascii.hexlify(secret)
-    doc = '<html><body>xxx<script type="text/javascript">%s;history.back()</script></body></html>' % (crypto_js + ';' + hotp_js + ';' + myotp_js.replace('FAFA',key))
+    doc = ''''<html>
+<body>
+<script type="text/javascript">%s;history.back()</script>
+</body>
+</html>''' % (crypto_js + ';' + hotp_js + ';' + myotp_js.replace('FAFA',key))
     return dataize(doc)
 
 if __name__ == '__main__':
